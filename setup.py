473c7978a1bab7a0028c2b6a2210df586652071f
Mar2zz <lasi.mar2zz@gmail.com>
2011-12-25 08:24:15 +0100

    fix for database not being created by setup.py (missing path)

--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
+import sys
+import os
+
+rundir = os.path.dirname(os.path.abspath(__file__))
+
+try:
+    frozen = sys.frozen
+except AttributeError:
+    frozen = False
+
+# Define path based on frozen state
+if frozen:
+    path_base = os.environ['_MEIPASS2']
+    rundir = os.path.dirname(sys.executable)
+    #path_base = os.path.dirname(sys.executable)
+else:
+    path_base = rundir
+
+# Include paths
+sys.path.insert(0, path_base)
+sys.path.insert(0, os.path.join(path_base, 'external'))
+
 try:
     from lib.database import *
 
